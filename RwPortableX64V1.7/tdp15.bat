e93524ba2bee8e155e17508c0bf4ccc3b1c39955
Jimmy Fung <jimmythefung@users.noreply.github.com>
2024-03-10 03:33:05 -0400

    For Windows

---
+++ RwPortableX64V1.7/tdp15.bat
@@ -0,0 +1,4 @@
+CD C:\RwPortableX64V1.7\Win64\Portable\
+Rw.exe /Min /Nologo /Stdout /Command="W16 0xFED170A8 0000"
+Rw.exe /Min /Nologo /Stdout /Command="W16 0xFED170AC 0000"
+Rw.exe /Min /Nologo /Stdout /Command="WRMSR 0x610 0x0 0x00DD8F00 0"
